3e7b9ff0b3ec652a0ba203ce0574cce42f14822f
phanhuytran <72870381+phanhuytran@users.noreply.github.com>
2021-10-08 10:53:01 +0700

    OFFICIAL | FINISHED PROJECT (90%) | 10:53 October 08, 2021

--- deliveryhuytrong/src/component/shipper-detail/shipper-detail-component/ShipperRatingSlider.js
+++ deliveryhuytrong/src/component/shipper-detail/shipper-detail-component/ShipperRatingSlider.js
@@ -16,14 +16,35 @@ export default function ShipperRatingSlider() {
     }, [rateList]);
 
     return (
-        <>
-            <Slider {...settingSlider}>
-                {
-                    rateList.map((rate, index) => {
-                        return <div key={index}><p>{rate.customer.username}</p></div>
-                    })
-                }
-            </Slider>
-        </>
+        <Slider {...settingSlider} className="shipper-rating-slider">
+            {
+                rateList.map((rate, index) => {
+                    return <div key={index} className="col-md-4 col-sm-4 col-xs-12 col-lg-4">
+                        <div className="single-about-us-bottom shipper-rating-slider-height">
+                            <div className="shipper-rating-info-1">
+                                <div>
+                                    <img src={rate.shipper.avatar} alt="avatar" />
+                                </div>
+                                <div>
+                                    <p>{rate.shipper.first_name}</p>
+                                    <p>{rate.shipper.last_name}</p>
+                                </div>
+                            </div>
+                            <div className="shipper-rating-info-2">
+                                <p>
+                                    <img src={rate.customer.avatar} alt="avatar" />
+                                    <span className="customer-rating-info-1">{rate.customer.username}:</span>
+                                    <span className="customer-rating-info-2">
+                                        <i style={{ fontSize: 20, color: '#f9bf3b' }} className="fas fa-star"></i>
+                                        <span>{rate.rate}</span><br />
+                                        <span style={{ fontSize: 15, marginLeft: '45px', color: '#5d5d5d' }}>{rate.content}</span>
+                                    </span>
+                                </p>
+                            </div>
+                        </div>
+                    </div>
+                })
+            }
+        </Slider>
     );
 }
